3b22351d320a2864a75b028bfce8297fd016f414
BowTiedCrocodile <BowTiedCrocodile@protonmail.com>
2023-08-04 19:54:23 -0400

    Initial commit

---
+++ src/app/links.tsx
@@ -0,0 +1,60 @@
+import { Fragment } from 'react'
+import { Popover, Transition } from '@headlessui/react'
+import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid'
+import {
+  ArrowPathIcon,
+  ChartPieIcon,
+  CursorArrowRaysIcon,
+  FingerPrintIcon,
+  SquaresPlusIcon,
+} from '@heroicons/react/24/outline'
+
+const solutions = [
+  { name: 'React', description: 'Understand your Web Framework', href: 'https://react.dev/', icon: ChartPieIcon },
+  { name: 'Next.js', description: 'Learn about your Static Site Rendering', href: 'https://nextjs.org/docs/getting-started/installation', icon: CursorArrowRaysIcon },
+  { name: 'TypeScript', description: "Comprehend your programming language", href: 'https://www.typescriptlang.org/', icon: FingerPrintIcon },
+  { name: 'Tailwind UI and CSS', description: 'See your UI Library components', href: 'https://tailwindui.com/components', icon: SquaresPlusIcon },
+  { name: 'Vercel', description: 'Host your UI online', href: 'https://vercel.com/', icon: ArrowPathIcon }
+]
+
+export default function Links() {
+  return (
+    <Popover className="relative">
+      <Popover.Button className="inline-flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900">
+        <span>Technical Links</span>
+        <ChevronDownIcon className="h-5 w-5" aria-hidden="true" />
+      </Popover.Button>
+
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-200"
+        enterFrom="opacity-0 translate-y-1"
+        enterTo="opacity-100 translate-y-0"
+        leave="transition ease-in duration-150"
+        leaveFrom="opacity-100 translate-y-0"
+        leaveTo="opacity-0 translate-y-1"
+      >
+        <Popover.Panel className="absolute left-1/2 z-10 mt-5 flex w-screen max-w-max -translate-x-1/2 px-4">
+          <div className="w-screen max-w-md flex-auto overflow-hidden rounded-3xl bg-white text-sm leading-6 shadow-lg ring-1 ring-gray-900/5">
+            <div className="p-4">
+              {solutions.map((item) => (
+                <div key={item.name} className="text-justify group relative flex gap-x-6 rounded-lg p-4 hover:bg-gray-50">
+                  <div className="mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
+                    <item.icon className="h-6 w-6 text-gray-600 group-hover:text-indigo-600" aria-hidden="true" />
+                  </div>
+                  <div>
+                    <a href={item.href} className="font-semibold text-gray-900">
+                      {item.name}
+                      <span className="absolute inset-0" />
+                    </a>
+                    <p className="mt-1 text-gray-600">{item.description}</p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </Popover.Panel>
+      </Transition>
+    </Popover>
+  )
+}
