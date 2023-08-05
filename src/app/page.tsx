9ca3d88fb6d26c7f0fef7b339a4d773e933d25e1
BowTiedCrocodile <BowTiedCrocodile@protonmail.com>
2023-08-04 20:07:45 -0400

    fix github link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -133,12 +133,12 @@ export default function Home() {
               </p>
               <div className="mt-10 flex items-center justify-center gap-x-6">
                 <a
-                  href="#"
+                  href="https://github.com/BowTiedCrocodile/Tailwind-Starter"
                   className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
                   Get started on Github
                 </a>
-                <a href="https://tailwindui.com/documentation" className="text-sm font-semibold leading-6 text-gray-900">
+                <a href="https://github.com/BowTiedCrocodile/Tailwind-Starter" className="text-sm font-semibold leading-6 text-gray-900">
                   Learn more <span aria-hidden="true">→</span>
                 </a>
               </div>
